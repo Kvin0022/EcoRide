45f44a32adc75f83bb745b69824b6e794f2def59
Kvin0022 <kvin002@hotmail.fr>
2025-05-14 23:37:10 +0200

    page html css js detail-covoiturage terminé

--- frontend/Projet_ecoride/Js/Accueil.js
+++ frontend/Projet_ecoride/Js/Accueil.js
@@ -9,3 +9,20 @@ document.addEventListener('DOMContentLoaded', function() {
         nav.classList.toggle('mobile-menu');
     });
 });
+
+// on attend que le DOM soit prêt
+document.addEventListener('DOMContentLoaded', () => {
+    // on cible toutes les images des cartes
+    const images = document.querySelectorAll('.card-img img');
+
+    images.forEach(img => {
+    img.addEventListener('mouseenter', () => {
+        // on agrandit très légèrement
+        img.style.transform = 'scale(1.05)';
+    });
+    img.addEventListener('mouseleave', () => {
+        // on remet à l’échelle normale
+        img.style.transform = 'scale(1)';
+    });
+    });
+});
--- frontend/Projet_ecoride/Js/Détail-covoiturage.js
+++ frontend/Projet_ecoride/Js/Détail-covoiturage.js
@@ -9,3 +9,24 @@ document.addEventListener('DOMContentLoaded', function() {
         nav.classList.toggle('mobile-menu');
     });
 });
+
+const overlay = document.getElementById('modal-overlay');
+const btnParticiper = document.querySelector('.btn-participer');
+const btnClose = document.querySelector('.modal-close');
+const btnCancel = document.getElementById('btn-cancel');
+
+function openModal() {
+    overlay.style.display = 'flex';
+}
+function closeModal() {
+    overlay.style.display = 'none';
+}
+
+btnParticiper.addEventListener('click', openModal);
+btnClose.addEventListener('click', closeModal);
+btnCancel.addEventListener('click', closeModal);
+
+// ferme aussi en cliquant en dehors de la boîte
+overlay.addEventListener('click', e => {
+    if (e.target === overlay) closeModal();
+});
