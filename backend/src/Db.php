8997f235fa6626718a137eb2263ea19ce572d26d
Kvin0022 <kvin002@hotmail.fr>
2025-09-22 10:44:59 +0200

    mise à jour db

--- backend/src/Db.php
+++ backend/src/Db.php
@@ -24,9 +24,4 @@ class Db {
   }
 }
 
-$dsn = "mysql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_NAME') . ";charset=utf8mb4";
-$pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'), [
-  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-]);
 
