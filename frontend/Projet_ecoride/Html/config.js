91d0c6841489ecf7e8d0844228e2e3f9e0629995
Kvin0022 <kvin002@hotmail.fr>
2025-09-22 19:47:03 +0200

    config.js

--- frontend/Projet_ecoride/Html/config.js
+++ frontend/Projet_ecoride/Html/config.js
@@ -1,7 +1,4 @@
 <script>
-  window.API_BASE_URL = (
-    location.hostname.endsWith('netlify.app')
-      ? 'https://ecoride-production-0838.up.railway.app'
-      : 'http://localhost:8080'
-  );
+  window.API_BASE_URL = 'https://ecoride-production-0838.up.railway.app';
+
 </script>
